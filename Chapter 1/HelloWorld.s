77e5d4a2e13f24616ac5799037303950b3b2f58d
rperson <person.rl@pm.me>
2024-01-28 21:52:20 +0000

    play with return code

--- Chapter 1/HelloWorld.s
+++ Chapter 1/HelloWorld.s
@@ -20,7 +20,7 @@ _start: mov	X0, #1	    // 1 = StdOut
 // and then call Linux to do it.
 	mov     X0, #0      // Use 0 return code
         mov     X8, #93      // Service command code 93 terminates this program
-        svc     #10           // Call linux to terminate the program
+        svc     #0           // Call linux to terminate the program
 
 .data
 helloworld:      .ascii  "Hello World!\n"
